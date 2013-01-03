7fdee63d4e96f2a47053b30943ec1b9538064c17
iamsk <iamsk.info@gmail.com>
2013-01-03 21:52:19 +0800

    update

--- dbtool/delete.py
+++ dbtool/delete.py
@@ -5,7 +5,11 @@ import hashlib
 dbdoorbot = "/home/pi/doorbot/doorbot.db"
 db = bdb.hashopen(dbdoorbot, 'c')
 password = hashlib.md5('853211').hexdigest()
+del_list = []
 for key in db:
     if db[key] == password:
-        del db[key]
+        del_list.append(key)
+
+for k in del_list:
+    del db[k]
 
